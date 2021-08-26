af9ffdc88f012afbe4659b939dc2313d9ed32775
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-26 09:09:57 +0530

    feat:Ability To Validate LastName Using Reflection

--- UserRegistration_Reflection/Validation.cs
+++ UserRegistration_Reflection/Validation.cs
@@ -7,9 +7,15 @@ namespace UserRegistration_Reflection
    public class Validation
     {
         public string RegexName = "^[A-Z]{1}[a-z]{3,}$";
+        public string RegexLastName = "^[A-Z][a-z]{2,}$";
         public void FirstName(string firstName)
         {
             if (Regex.IsMatch(firstName, RegexName) == false)
+                throw new ExceptionHandling(ExceptionHandling.ExceptionsCustomMessage.INVALID_INPUT, " : Name start with Capital and has Minimum 3 characters");
+        }
+        public void LastName(string lastName)
+        {
+            if (Regex.IsMatch(lastName, RegexLastName) == false)
                 throw new ExceptionHandling(ExceptionHandling.ExceptionsCustomMessage.INVALID_INPUT, " : Name start with Capital and has Minimum 3 characters");
         }
     }
