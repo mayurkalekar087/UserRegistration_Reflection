313cf36daf3f65a6c35dfee369f37de1832251e1
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-26 09:33:31 +0530

    feat:Ability To Validate PasswordRule1 Using Reflection

--- UserRegistration_Reflection/Validation.cs
+++ UserRegistration_Reflection/Validation.cs
@@ -10,6 +10,7 @@ namespace UserRegistration_Reflection
         public string RegexLastName = "^[A-Z][a-z]{2,}$";
         public string RegexEmail = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
         public string RegexMobileNumber = "^[0-9]{2}[ ][1-9][0-9]{9}$";
+        public string RegexPasswordRule1 = "^[a-zA-Z]{8,}$";
         public void FirstName(string firstName)
         {
             if (Regex.IsMatch(firstName, RegexName) == false)
@@ -29,6 +30,11 @@ namespace UserRegistration_Reflection
         {
             if (Regex.IsMatch(mobileNumber, RegexMobileNumber) == false)
                 throw new ExceptionHandling(ExceptionHandling.ExceptionsCustomMessage.INVALID_INPUT, " : Ivalid MobileNumber");
+        }
+        public void PasswordRule1(string passwordRule1)
+        {
+            if (Regex.IsMatch(passwordRule1, RegexPasswordRule1) == false)
+                throw new ExceptionHandling(ExceptionHandling.ExceptionsCustomMessage.INVALID_INPUT, " : Ivalid PasswordRule1");
         }
     }
 }
