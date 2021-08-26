8ab79c7624f9b0405331ce8e3c5ab173d348ca6c
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-26 09:16:27 +0530

    feat:Ability To Validate EmailId Using Reflection

--- UserRegistration_Reflection/Validation.cs
+++ UserRegistration_Reflection/Validation.cs
@@ -8,6 +8,7 @@ namespace UserRegistration_Reflection
     {
         public string RegexName = "^[A-Z]{1}[a-z]{3,}$";
         public string RegexLastName = "^[A-Z][a-z]{2,}$";
+        public string RegexEmail = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
         public void FirstName(string firstName)
         {
             if (Regex.IsMatch(firstName, RegexName) == false)
@@ -17,6 +18,11 @@ namespace UserRegistration_Reflection
         {
             if (Regex.IsMatch(lastName, RegexLastName) == false)
                 throw new ExceptionHandling(ExceptionHandling.ExceptionsCustomMessage.INVALID_INPUT, " : Name start with Capital and has Minimum 3 characters");
+        }
+        public void Email(string email)
+        {
+            if (Regex.IsMatch(email, RegexEmail) == false)
+                throw new ExceptionHandling(ExceptionHandling.ExceptionsCustomMessage.INVALID_INPUT, " : Ivalid EmailId");
         }
     }
 }
